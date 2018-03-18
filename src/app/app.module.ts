5a2df0742d20b44e4b75fde77137fa4f65a69261
Narain Mittal <mittalnarain123@gmail.com>
2018-03-18 16:23:25 -0700

    Add RouterModule and application routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule, Routes } from '@angular/router';
 import 'hammerjs';
 
 
@@ -12,20 +13,32 @@ import { MazeOptionsComponent } from './components/maze-options/maze-options.com
 import { MazeDisplayComponent } from './components/maze-display/maze-display.component';
 import { MazeComponent } from './components/maze/maze.component';
 import { MazeService } from './services/maze.service';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MazeComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MazeOptionsComponent,
     MazeDisplayComponent,
-    MazeComponent
+    MazeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     HttpClientModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [MazeService],
   bootstrap: [AppComponent]
